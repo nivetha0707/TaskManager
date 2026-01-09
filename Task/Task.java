11cb177b0ddb3b332f301cb958fd4a40fe3f5b6a
Nivetha S <nivethaaa2007@gmail.com>
2026-01-09 09:31:14 +0530

    Add files via upload

---
+++ Task/Task.java
@@ -0,0 +1,18 @@
+package Task;
+
+public class Task {
+    private String taskdescription;
+
+    public Task(String taskdescription)
+    {
+        this.taskdescription=taskdescription;
+    }
+
+    public String getTaskdescription() {
+        return taskdescription;
+    }
+
+    public void setTaskdescription(String taskdescription) {
+        this.taskdescription = taskdescription;
+    }
+}
